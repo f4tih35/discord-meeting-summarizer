1fcaee87e84f2ff8e74e7d5efb8299537b08d181
Fatih Melih Ersoy <fatihmelih9@gmail.com>
2024-01-27 23:44:35 +0300

    Create process.js

---
+++ bot/commands/voice/process.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
+const { apiEndpoint } = require('../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('process')
+        .setDescription('Process the recording.')
+        .addIntegerOption(option =>
+            option.setName('id')
+            .setDescription('The ID to process')
+            .setRequired(true)),
+    async execute(interaction) {
+        const id = interaction.options.getInteger('id');
+        await interaction.reply(`Processing started for ID: ${id}...`);
+        try {
+            const response = await axios.get(`${apiEndpoint}/process/${id}`);
+            await interaction.editReply(`OK: ${JSON.stringify(response.data)}`);
+        } catch (error) {
+            console.error('Error:', error);
+            await interaction.editReply('There was an error trying to process recording!');
+        }
+    },
+};
